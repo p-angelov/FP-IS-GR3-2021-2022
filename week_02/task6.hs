5c98c69b1481039cd3da72c43d54ae0690fc32a1
pephistopheles <phangelov@gmail.com>
2022-03-05 22:36:52 +0200

    Create task6.hs

---
+++ week_02/task6.hs
@@ -0,0 +1,19 @@
+main :: IO()
+main = do
+    --A number is interesting if and only if 
+    --it is evenly divided by the sum of its digits.
+    print $ isInteresting 410 == True
+    print $ isInteresting 212 == False
+    print $ isInteresting 567 == False
+    print $ isInteresting 70 == True 
+    print $ isInteresting 5 == True 
+    print $ isInteresting 4 == True
+
+sumDigits :: Int -> Int
+sumDigits 0 = 0
+sumDigits n = mod n 10 + sumDigits (div n 10)
+
+isInteresting :: Int -> Bool
+isInteresting n
+  | mod n (sumDigits n) == 0 = True
+  | otherwise = False
