f9741188df7ca98b0eab273d705dd99e4b1dafc7
pephistopheles <phangelov@gmail.com>
2022-03-09 18:40:51 +0200

    Fixed task 10

--- week_02/task10.hs
+++ week_02/task10.hs
@@ -3,13 +3,6 @@ main = do
     print $ countPalindromes 5 13 == 5 -- 6 7 8 9 11
     print $ countPalindromes 13 5 == 5 -- 6 7 8 9 11
 
-{-
-Проблемът е бездънна рекурсия. Направи helper да работи при предположението,
-че първият му аргумент е по-малък или равен на втория.
-След това се опитай от главната функция да подадеш по-малкото от двете числа за първи аргумент и по-голямото за втори.
-
-2. Опитай се да имаш най-много две условия в гард.
--}
 rev :: Int -> Int
 rev n = helper n 0
  where
@@ -22,10 +15,12 @@ isPalindrome x = rev x == x
 
 countPalindromes :: Int -> Int -> Int
 countPalindromes a b
-  | a > b = countPalindromes b a
-  | otherwise = helper a b 0
+  | a < 0 = error "value lower than 0"
+  | b < 0 = error "value lower than 0"
+  | otherwise = helper (min a b) (max a b) 0
   where
       helper :: Int -> Int -> Int -> Int
       helper a b count
-       | a <= b && isPalindrome (a + 1) = count + 1 
+       | a == b = count
+       | a < b && isPalindrome (a + 1) = helper (a + 1) b (count + 1) 
        | otherwise = helper (a + 1) b count
