ad759da7cefa5b3f7cbceb9835aec83474511af6
pephistopheles <phangelov@gmail.com>
2022-03-13 18:43:20 +0200

    Added working task 5

---
+++ week_03/task5.hs
@@ -0,0 +1,11 @@
+main :: IO()
+main = do
+    print $ p 1 == 1
+    print $ p 2 == 5
+    print $ p 3 == 12
+    print $ p 4 == 22
+    print $ p 5 == 35
+    print $ p 6 == 51
+
+p :: Int -> Int
+p n = div (3 * n * n - n) 2
