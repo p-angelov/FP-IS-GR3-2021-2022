a5712ed2704b0ece1c3c24a1e72c04146a0fc867
pephistopheles <phangelov@gmail.com>
2022-04-13 14:14:23 +0300

    Fixed week 6 classwork

--- Weekly/week_06/Classwork/task2.hs
+++ Weekly/week_06/Classwork/task2.hs
@@ -1,5 +1,3 @@
-import Data.Char
-import Data.List
 main :: IO()
 main = do
     print $ dominates (\x -> x + 1) (\x -> x + 2) [1, 2, 3, 4, 5] == False
@@ -8,8 +6,8 @@ main = do
     print $ dominatesFold (\x -> x + 1) (\x -> x + 2) [1, 2, 3, 4, 5] == False
     print $ dominatesFold (\x -> x * 3) (\x -> x + 2) [1, 2, 3, 4, 5] == True
 
-dominates :: (Ord a) => (a -> a) -> (a -> a) -> [a] -> Bool
-dominates f g xs = all (\ x -> f x >= g x) xs
-
 dominatesFold :: (Ord a) => (a -> a) -> (a -> a) -> [a] -> Bool
-dominatesFold f g xs = foldl () (\ acc x -> acc && f x >= g x)
+dominatesFold f g xs = foldl (\ acc x -> acc && f x >= g x) True xs
+
+dominates :: (Ord a) => (a -> a) -> (a -> a) -> [a] -> Bool
+dominates f g = all (\ x -> f x >= g x)
--- Weekly/week_06/Classwork/task5.hs
+++ Weekly/week_06/Classwork/task5.hs
@@ -1,17 +1,25 @@
-import Data.Char
 import Data.List
+
 main :: IO()
 main = do
-    print $ hardestSubject [("John", "Maths", 5), ("Kennedy", "English", 2), ("Joe", "Programming", 4), ("Claudia", "Programming", 6), ("Sam", "Maths", 4), ("Jenn", "English", 2)] == "English"
-    print $ hardestSubject [("John", "Maths", 5), ("Kennedy", "English", 5), ("Joe", "Programming", 4), ("Claudia", "Programming", 6), ("Sam", "Maths", 4), ("Jenn", "English", 5)] == "Maths"
+    print $ hardestSubjectFold [("John", "Maths", 5), ("Kennedy", "English", 2), ("Joe", "Programming", 4), ("Claudia", "Programming", 6), ("Sam", "Maths", 4), ("Jenn", "English", 2)] == "English"
+    print $ hardestSubjectFold [("John", "Maths", 5), ("Kennedy", "English", 5), ("Joe", "Programming", 4), ("Claudia", "Programming", 6), ("Sam", "Maths", 4), ("Jenn", "English", 5)] == "Maths"
 
 type Student = String
 type Subject = String
 type Note = Double
 type Record = (Student, Subject, Note)
 
+hardestSubjectFold :: [Record] -> Subject
+hardestSubjectFold xs = foldl1 (\ x y -> if getAvgNs x > getAvgNs y then y else x) $ getUniqSubj xs
+ where
+     getAvgNs s = sum (getNs s) / (fromIntegral $ length (getNs s))
+     getNs s = [ n | (_, c, n) <- xs, c == s] -- filter (\ (_, c, _) -> c == s) xs
+     getUniqSubj xs = nub $ map (\ (_, s, _) -> s) xs
+
 hardestSubject :: [Record] -> Subject
-hardestSubject xs = getUniqSubj xs
-  where
-      getAvg
-      getUniqSubj xs = nub $ map (\ (_, s, _) -> s)
+hardestSubject xs = fst $ head $ sortOn snd $ map (\ s -> (s, getAvgNs s)) $ getUniqSubj xs
+ where
+     getAvgNs s = sum (getNs s) / (fromIntegral $ length (getNs s))
+     getNs s = [ n | (_, c, n) <- xs, c == s] -- filter (\ (_, c, _) -> c == s) xs
+     getUniqSubj xs = nub $ map (\ (_, s, _) -> s) xs
